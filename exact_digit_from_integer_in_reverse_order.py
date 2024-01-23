deb8acb741f800e0e4e06e146b712c9952cb6159
Jossel Redoquerio <josselgredoquerio@iskolarngbayan.pup.edu.ph>
2024-01-23 12:40:17 +0800

    name.append

--- exact_digit_from_integer_in_reverse_order.py
+++ exact_digit_from_integer_in_reverse_order.py
@@ -1,3 +1,15 @@
 # Exercise 11: Write a Program to extract each digit from an integer in the reverse order.
 # For example, If the given int is 7536, the output shall be “6 3 5 7“, 
 # with a space separating the digits.
+
+name = list()
+name.append("1")
+name.append("6")
+name.append("8")
+name.append("9")
+name.append("3")
+
+name.reverse()
+
+for name in name:
+    print(name)
